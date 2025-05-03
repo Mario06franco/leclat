cb052c9776cdf87b87d6c2b7a7faff4d2e234d37
Juan Jose Giraldo <juanjosegiraldo1001@gmail.com>
2025-05-02 19:32:39 -0500

    ipdate

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
+const env = loadEnv(mode, process.cwd());
 
 
 export default defineConfig({
+
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: backendUrl,
+        target: env.VITE_BACKEND_URL,
         changeOrigin: true,
         secure: false,
         
