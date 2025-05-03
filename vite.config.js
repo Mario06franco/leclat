56333c666b7feff24a3c63ca911e28e3186f172f
Juan Jose Giraldo <juanjosegiraldo1001@gmail.com>
2025-05-02 19:34:25 -0500

    env

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
-const env = loadEnv(mode, process.cwd());
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-export default defineConfig({
-
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: env.VITE_BACKEND_URL,
-        changeOrigin: true,
-        secure: false,
-        
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-})
+  };
+});
