15c31368988da3b3a75ab345a546237e9120ac00
Mario06franco <Mariofraco93@gmail.com>
2025-05-06 23:26:34 -0500

    agregue render

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './context/UserContext'
+console.log('ENV Variables:', import.meta.env) // ← Ver TODAS las variables
+console.log('Backend URL:', import.meta.env.VITE_BACKEND_URL) // ← Ver específicamente tu URL
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
